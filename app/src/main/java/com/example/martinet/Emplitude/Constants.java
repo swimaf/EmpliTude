c932cb57d0b88d08cb5ea55827b2cec9c96255a6
ArnaudRegnier <arnaud_r@hotmail.fr>
2016-01-20 18:55:12 +0100

    correction bug du commit

--- app/src/main/java/com/example/martinet/Emplitude/Constants.java
+++ app/src/main/java/com/example/martinet/Emplitude/Constants.java
@@ -9,11 +9,11 @@ public class Constants {
     final static public String store = System.getenv("EXTERNAL_STORAGE");
     final static public String courFile ="ADE.cours";
     final static public String identifiantFile = ".identifiant.txt";
-<<<<<<< HEAD
+
     final static public int intervaleSonnerieRepeter = 1;
-=======
+
     final static public String tacheFile = "tache.txt";
->>>>>>> 30f546520b816cfbbaecc455360cf12ca20c25f1
+
 
     public static Boolean CONNECTED(Context context) {
         ConnectivityManager cm =(ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
